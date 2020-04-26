38e3a1600ab03faf9ef80efbf9a6e93d196313f9
OdZarref <ludwigvonmarcelo@gmail.com>
2020-04-26 00:23:36 -0300

    Adicionando função verificar_mmc()

--- js/functions.js
+++ js/functions.js
@@ -53,6 +53,20 @@ function calcular_primo(possivel_primo) {
     }
 }
 
+function verificar_mmc() {
+    numero1 = document.querySelector('input#caixa_texto_numero1')
+    numero2 = document.querySelector('input#caixa_texto_numero2')
+    resposta = document.querySelector('p#resposta')
+
+    if (numero1.value.length == 0 || numero2.value.length == 0) {
+        resposta.innerHTML = 'Preencha os dois campos!'    
+    } else if (numero1.value.length < 0 || numero2.value.length < 0) {
+        resposta.innerHTML = 'Os números devem ser positivos!'
+    } else {
+        resposta.innerHTML = mmc(numero1.value, numero2.value)
+    }
+}
+
 function mmc(a, b) {
     return a * (b / mdc(a, b))
 }
